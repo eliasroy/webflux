38da603171283a441f6e8d50c902edaaad9d5c82
elias provincia <provinciaelias@gmail.com>
2024-11-07 11:45:02 -0500

    feat: flux, suscribe and error

--- src/main/java/com/webflux/webflux/WebfluxApplication.java
+++ src/main/java/com/webflux/webflux/WebfluxApplication.java
@@ -1,13 +1,30 @@
 package com.webflux.webflux;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Flux;
+
+import java.util.logging.Logger;
 
 @SpringBootApplication
-public class WebfluxApplication {
+public class WebfluxApplication implements CommandLineRunner {
+	private static final Logger log = Logger.getLogger(WebfluxApplication.class.getName());
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebfluxApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Flux<String> nombres = Flux.just("", "Pedro Fulano", "Maria Fulana", "Diego Sultano", "Juan " +
+				"Mengano", "Bruce Lee").doOnNext(e->{
+			if(e.isEmpty()){
+				throw new RuntimeException("Nombre vacio");
+			}
+			System.out.println(e);
+		});
+		nombres.subscribe(e -> log.info(e),error->log.info(error.getMessage()));
+
+	}
 }
